60a34df0c348cb096d326154cf3d27b6cff65be6
Niranjan <rathinamniranjan@gmail.com>
2023-06-15 21:14:00 +0530

    Insert Operation

--- src/main/kotlin/linkedList/LinkedList.kt
+++ src/main/kotlin/linkedList/LinkedList.kt
@@ -57,5 +57,35 @@ class LinkedList<T> {
         return this
     }
 
+    fun nodeAt(index:Int):Node<T>?{
+
+        var currentNode = head
+        var currentIndex = 0
+
+        while (currentNode!=null&&currentIndex < index){
+
+            currentNode = currentNode.next
+            currentIndex++
+
+        }
+        return currentNode
+
+    }
+
+    fun insert(value: T,afterNode:Node<T>):Node<T>{
+
+        if(tail == afterNode){
+
+            append(value)
+            return tail!!
+
+        }
+        var newNode = Node(value = value, next = afterNode.next)
+        afterNode.next = newNode
+        size++
+        return newNode
+
+    }
+
 
 }
--- src/main/kotlin/linkedList/Main.kt
+++ src/main/kotlin/linkedList/Main.kt
@@ -3,8 +3,15 @@
 fun main() {
 
     val linkedList = LinkedList<Int>()
+    //Pushing
     linkedList.push(3).push(2).push(1).push(0)
-
+    //Appending
     println(linkedList.append(4).append(5))
+    //Finding the node
+    println(linkedList.nodeAt(3))
+    var nodeAt = linkedList.nodeAt(3)!!
+    //Inserting the Node
+    linkedList.insert(10,nodeAt)
+    println(linkedList)
 
 }
